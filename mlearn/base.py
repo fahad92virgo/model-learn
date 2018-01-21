6ed8790ef68185459798058078e350b1e2e4d53d
Fahad Sarfraz <fahad.sarfraz@outlook.com>
2018-01-21 17:30:04 +0500

    ridge regression model + tutorial added

--- mlearn/base.py
+++ mlearn/base.py
@@ -104,3 +104,40 @@ def regression_gradient_descent(feature_matrix, output, initial_weights, step_si
             converged = True
 
     return weights
+
+
+# --------------------------------------------------------------------------------------------------------------------
+# Ridge Regression Implementation
+# --------------------------------------------------------------------------------------------------------------------
+def feature_derivative_ridge(errors, feature, weight, l2_penalty, feature_is_constant):
+    if feature_is_constant:
+        derivative = 2 * np.dot(errors, feature)
+    else:
+        derivative = 2 * np.dot(errors, feature) + 2 * l2_penalty * weight
+    return derivative
+
+
+def ridge_regression_gradient_descent(feature_matrix, output, initial_weights, step_size, l2_penalty,
+                                      max_iterations=100):
+    weights = np.array(initial_weights)  # make sure it's a numpy array
+
+    # while not reached maximum number of iterations:
+    for n in range(max_iterations):
+
+        # compute the predictions based on feature_matrix and weights using your predict_output() function
+        predictions = predict_output(feature_matrix, weights)
+        # compute the errors as predictions - output
+        errors = predictions - output
+        for i in range(len(weights)):  # loop over each weight
+            # Recall that feature_matrix[:,i] is the feature column associated with weights[i]
+            # compute the derivative for weight[i].
+            # (Remember: when i=0, you are computing the derivative of the constant!)
+            if i == 0:
+                derivative = feature_derivative_ridge(errors, feature_matrix[:, i], weights[i], l2_penalty, True)
+            else:
+                derivative = feature_derivative_ridge(errors, feature_matrix[:, i], weights[i], l2_penalty, False)
+            # subtract the step size times the derivative from the current weight
+
+            weights[i] = weights[i] - (step_size * derivative)
+
+    return weights
---
+++ tutorials/ridge_regression.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+from mlearn.base import get_numpy_data, ridge_regression_gradient_descent, predict_output
+
+# Load in house sales data
+# Dataset is from house sales in King County, the region where the city of Seattle, WA is located
+
+dtype_dict = {'bathrooms': float, 'waterfront': int, 'sqft_above': int, 'sqft_living15': float, 'yr_renovated': int,
+              'grade': int, 'price': float, 'bedrooms': float, 'zipcode': str, 'long': float, 'sqft_lot15': float,
+              'sqft_living': float, 'floors': str, 'condition': int, 'lat': float, 'date': str, 'sqft_basement': int,
+              'yr_built': int, 'id': str, 'sqft_lot': int, 'view': int}
+
+
+df_train_data = pd.read_csv('data/kc_house_train_data.csv', dtype=dtype_dict)
+df_test_data = pd.read_csv('data/kc_house_test_data.csv', dtype=dtype_dict)
+
+# set feature list and target variable
+lst_features = ['sqft_living', 'sqft_living15']
+target_variable = 'price'
+
+
+# get features matrix and output vector
+train_feature_matrix, train_output = get_numpy_data(df_train_data, lst_features, target_variable)
+
+
+# set initial weights and learning parameters
+initial_weights = np.array([-100000., 1., 1.])
+step_size = 1e-12
+l2_penalty = 0
+max_iterations = 1000
+
+# train model
+weights_model = ridge_regression_gradient_descent(train_feature_matrix, train_output, initial_weights,
+                                                  step_size, l2_penalty, max_iterations)
+print('feature weights: %s' % weights_model)
+
+# evaluate mode performance
+test_feature_matrix, test_output = get_numpy_data(df_test_data, lst_features, target_variable)
+predictions_model = predict_output(test_feature_matrix, weights_model)
+rss_model = np.sum(np.square(test_output - predictions_model))
+print('Residual Sum of Squares: %s' % rss_model)
