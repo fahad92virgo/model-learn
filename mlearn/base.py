2c2c71afe0c69fcdb397f3d87f54d6fa0016bd22
Fahad Sarfraz <fahad.sarfraz@outlook.com>
2018-01-21 16:32:26 +0500

    base methods added

---
+++ mlearn/base.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+
+
+def get_numpy_data(dataframe, features, output):
+
+    """
+    Parameters
+    ----------
+    :param dataframe: pandas dataframe containing the features and target variable
+    :param features: list of features containing the names of the columns to be included as features
+    :param output: name of the target variable as a string
+
+
+    Returns
+    ---------
+    :return: feature_matrix: numpy matrix containing the features as columns
+    :return: output_array: numpy array with the target variable values
+    """
+
+    # add a constant column to a Pandas Data Frame
+    dataframe['constant'] = 1
+
+    # add the column 'constant' to the front of the features list so that we can extract it along with the others:
+    features = ['constant'] + features
+
+    # select the columns of dataFrame given by the features list:
+    features_dataframe = pd.DataFrame(dataframe, columns=features)
+
+    # convert the features_dataFrame into a numpy matrix:
+    feature_matrix = features_dataframe.as_matrix()
+
+    # assign the column of dataframe associated with the output to a numpy array
+    output_array = dataframe[output].values
+
+    return feature_matrix, output_array
+
+
+def predict_output(feature_matrix, weights):
+    """
+    Parameters
+    ----------
+    :param feature_matrix: numpy matrix containing the features as columns
+    :param weights: corresponding numpy array containing the weight of each feature
+
+    :return: numpy array containig the predictions
+    """
+
+    predictions = np.dot(feature_matrix, weights)
+
+    return predictions
